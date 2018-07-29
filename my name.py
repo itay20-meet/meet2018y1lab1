0cee6b288ed85afb590daf154afd706f268c89f3
itay20-meet <itay20@meet.mit.edu>
2018-07-29 11:13:27 -0400

    Stopping here

---
+++ my name.py
@@ -0,0 +1,18 @@
+import turtle
+turtle.penup()
+turtle.goto(-200,-100)
+turtle.pendown()
+turtle.goto(-200,100)
+turtle.goto(-250,100)
+turtle.goto(-150,100)
+turtle.penup()
+turtle.goto(-250,-100)
+turtle.pendown()
+turtle.goto(-150,-100)
+turtle.penup()
+turtle.goto(-50,-100)
+turtle.pendown()
+turtle.goto(-50,100)
+turtle.goto(-100,100)
+turtle.goto(0,100)
+
